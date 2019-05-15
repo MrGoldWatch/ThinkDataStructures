61edff18d3e3ae1056a7919d2c36626107b4dfc8
Hosein Khosravani - PC 10 <Hosein.Khosravani@gmail.com>
2019-05-15 10:33:33 -0700

    working on exercise 5 - realized there is a WikiParser class to use for checking valid links

--- code/src/com/allendowney/thinkdast/WikiNodeExample.java
+++ code/src/com/allendowney/thinkdast/WikiNodeExample.java
@@ -21,7 +21,8 @@ import packages.*;
 public class WikiNodeExample {
 	
 	public static void main(String[] args) throws IOException {
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+        String url = "https://en.wikipedia.org/wiki/Java";
+        System.out.println();
 		
 		// download and parse the document
 		Connection conn = Jsoup.connect(url);
@@ -33,33 +34,47 @@ public class WikiNodeExample {
 		// TODO: avoid selecting paragraphs from sidebars and boxouts
         Elements paras = content.select("p");
         // System.out.println(paras);
-        Element firstPara = paras.get(1);
+
+        Element firstPara = paras.get(0);
         // System.out.println(firstPara);
         
         // To get the first element which has tag <a>
         Elements links = paras.select("a");
         Element firstLink = links.get(0);
         
+        // Cant do this becuase of the link restrictions
+        // Can be replaced by the attr(key) method from Node class inside the below for loop
         // To get the value of the href attribute
         Attributes attrb = firstLink.attributes();
         String hrefAttrb = attrb.get("href");
-        System.out.println(firstLink);
-        System.out.println(attrb);
-        System.out.println(hrefAttrb);
-		// recursiveDFS(firstPara);
+        // System.out.println(firstLink); // prints the first accurance of <a> tag
+        // System.out.println(attrb); // prints the attributes of the first <a> tag
+        // System.out.println(hrefAttrb); // prints the value for attribute key "href"
+        
+        
+        // do i need to any DFS??
+        // recursiveDFS(firstPara);
 		System.out.println();
 
 		// iterativeDFS(firstPara);
 		System.out.println();
 
-        Iterable<Node> iter = new WikiNodeIterable(firstLink);
-        // System.out.print(iter);
+        // why do i need this? 
+        // to use class Node's attr(key) method
+        Iterable<Node> iter = new WikiNodeIterable(firstPara);
+        System.out.print(iter);
 		for (Node node: iter) {
-			// if ((node instanceof TextNode)) {
-			// 	System.out.print(node);
-            // }  
+
+            // System.out.println();
+
+            // String nodeAttrb = node.attr("href");
+            // System.out.println(nodeAttrb);
+            // System.out.println();
+			if ((node instanceof TextNode)) {
+				// System.out.print(node);
+            }  
             if (node instanceof Element) {
-                System.out.println(node);
+                // System.out.println(node);
             }
 		}
 	}
